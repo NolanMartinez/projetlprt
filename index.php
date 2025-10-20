21d23869c8d78919a2488e8b0fbb911526877586
juliendaux <92355450+juliendaux@users.noreply.github.com>
2025-10-20 13:45:48 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,9 +5,44 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+	<style>
+		#plans{
+			background-color : #ddddee;
+			height : 300px;
+			width : 456px;
+			border: solid 2px #ddddee;
+			border-radius : 10px;
+
+		}
+		#img-1{
+			height: 300px;
+			position : absolute;
+			border-radius : 10px;
+		}
+		#img-2{
+			height:35px;
+			position : relative;
+			top : 240px;
+			left : 420px;
+
+		}
+	</style>
 </head>
 <body>
     <?php
+        $verif = null;
+        if (!empty($_POST['capteur'])){
+            $id_cap = $_POST['capteur'];
+        }
+        else{
+            $id_cap = "1";
+        }
+                if (!empty($_POST['date'])){
+            $id_date = $_POST['date'];
+        }
+        else{
+            $id_date = "defaut";
+        }
         try {
             $pdo = new PDO("mysql:host=localhost;port=3306;dbname=projet_gps", "root");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -16,13 +51,23 @@
         }
     ?>
     
-	<form method="post">
+	<form method="post" id="info">
+        <script>
+            function envoie() {
+                document.forms["info"].submit();
+            }
+        </script>
         <label for="capteur">Choisissez un capteur :</label>
-        <select id="capteur" name="capteur">
+        <select id="capteur" name="capteur" onchange="envoie()">
             <?php
             $sql_cap = "SELECT * FROM capteur";
             foreach ($pdo->query($sql_cap) as $row) {
-                echo '<option value="';
+                if ($row['Id_capteur'] == $id_cap){
+                    echo '<option selected value="';
+                }
+                else{
+                    echo '<option value="';
+                }
                 echo $row['Id_capteur'];
                 echo '">';
                 echo $row['nom'];
@@ -30,16 +75,28 @@
             }
             ?>
         </select>
-		<label for="choix">Choisissez une date :</label>
-        <select id="choix" name="choix">
-            <option value="option1" selected>Maintenant</option>
-            <option value="option2">07/10/2025</option>
+		<label for="date">Choisissez une date :</label>
+        <select id="date" name="date" onchange="envoie()">
+            <option value="defaut">Maintenant</option>
+            <?php
+            $sql_date = "SELECT * FROM donnees WHERE Id_capteur = '$id_cap'";
+            foreach ($pdo->query($sql_date) as $row) {
+                if ($row['Id_donnees'] == $id_date){
+                    echo '<option selected value="';
+                }
+                else{
+                    echo '<option value="';
+                }
+                echo $row['Id_donnees'];
+                echo '">';
+                echo $row['date_donnees'];
+                echo '</option>';
+            }
+            ?>
         </select>
         <input type="submit" value="valider">
     </form>
     <?php
-    $id_cap = $_POST['capteur'];
-    
     $sql = "SELECT * FROM donnees WHERE Id_capteur = '$id_cap' ORDER BY Id_donnees DESC LIMIT 1";
     foreach ($pdo->query($sql) as $row) {
         echo '<p>x = ';
@@ -56,6 +113,10 @@
     }
 
     ?>
+    <div id="plans">
+	<img id="img-1" src="bat_ab_3.png" >
+	<img id="img-2" src="position.png" >
+    </div>
 
 </body>
 </html>
