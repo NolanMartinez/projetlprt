c9626ff73a3f41e3ec326fc966a4cacd0e9fda84
juliendaux <92355450+juliendaux@users.noreply.github.com>
2025-10-24 10:19:39 +0200

    resolution

--- test/script_page.js
+++ test/script_page.js
@@ -1,42 +1,6 @@
 function deco() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 85cdedd (j)
-    fetch('logout.php')
-        .then(() => {
-            window.location.href = 'index.php';
-        });
-<<<<<<< HEAD
-=======
+
     document.cookie = "id=";
     document.cookie = "mdp=;";
     window.location.reload();
->>>>>>> parent of 67b6c05 (a supprimer)
-=======
-    document.cookie = "id=";
-    document.cookie = "mdp=;";
-    window.location.reload();
->>>>>>> parent of 67b6c05 (a supprimer)
-=======
-    document.cookie = "id=";
-    document.cookie = "mdp=;";
-    window.location.reload();
->>>>>>> parent of 67b6c05 (a supprimer)
-=======
->>>>>>> parent of 85cdedd (j)
-=======
-    document.cookie = "id=";
-    document.cookie = "mdp=;";
-    window.location.reload();
->>>>>>> parent of 67b6c05 (a supprimer)
-=======
-    document.cookie = "id=";
-    document.cookie = "mdp=;";
-    window.location.reload();
->>>>>>> parent of 67b6c05 (a supprimer)
 }
